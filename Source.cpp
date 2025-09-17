610446013949c6075528ef605bd83c589e4d00a3
SirChili <thomastony4628@gmail.com>
2025-09-17 13:48:00 -0500

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,61 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+struct BreastCancerRecord {
+
+	int age;
+	double BMI;
+	double insulin;
+	bool classification;
+
+};
+
+std::vector<std::string> splitRecord_withStringStream(const std::string& record, char delimiter) {
+
+	std::vector<std::string > fields;
+	std::stringstream ss(record);
+	std::string field;
+
+	while (std::getline(ss, field, delimiter)) {
+		fields.push_back(field);
+	}
+	return fields;
+}
+
+int main() {
+	
+	std::ifstream inputFile("C://Users//A00593870//Documents//DataSet//dataR2.csv");
+
+	if (!inputFile) {
+
+		std::cout << "File Not Found.";
+		return -1;
+	}
+
+	std::string currentLine;
+
+	while (std::getline(inputFile, currentLine)) {
+		std::cout << currentLine << "\n\n\n";
+
+		auto splitRecord = splitRecord_withStringStream(currentLine, '.');
+
+		BreastCancerRecord currentRecord = {
+
+			std::stoi(splitRecord[0]),
+			std::stoi(splitRecord[1]),
+			std::stoi(splitRecord[2]),
+			std::stoi(splitRecord[3]),
+
+		};
+
+		int a;
+
+	}
+	
+
+}
